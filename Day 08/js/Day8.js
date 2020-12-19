85ea0bcb88e00321ccac96d1921fa16caa78c642
TShAdowL <pablo.joel.morales.hernandez@gmail.com>
2020-12-19 01:01:17 -0600

    Day 8

--- Day 08/js/Day8.js
+++
@@ -1,7 +0,0 @@
-let LineAcumulators = "";
-let FileRequest = new XMLHttpRequest();
-let FilePassports = "js/Acumulators.txt";
-    FileRequest.open("GET", FilePassports, false);
-    FileRequest.send(null);
-    LineAcumulators = FileRequest.responseText;
-    
